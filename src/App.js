cabd2ecf4b7643a19c7dc37a3b37102ab746292e
Sapna190 <sapnasharma8849711@gmail.com>
2024-05-24 14:17:15 +0530

    project-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+//import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="Text Analyzer" home="Home" about="About Us"/>
+    <div className='container   my-3' >
+    {/*<TextForm heading="Enter text to analyze"/>*/}
+    <About/>
     </div>
+</>
   );
 }
 
