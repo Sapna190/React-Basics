b789b661472d2003d1db42a5ab47699978340572
Sapna190 <sapnasharma8849711@gmail.com>
2024-05-27 22:58:04 +0530

    added some features

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,9 +5,11 @@ export default function TextForm() {
   return (
     <div>
         <div className="mb-3">
-            <h2 className='heading my-3 ' style={myStyle}>TextUtils to analyze your text.</h2>
+            <h2 className='heading my-3 ' >TextUtils to analyze your text.</h2>
             <label htmlFor="exampleFormControlTextarea1" className="form-label">Example</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <button>button1</button>
+            <button>button2</button>
         </div>
     </div>
   )
