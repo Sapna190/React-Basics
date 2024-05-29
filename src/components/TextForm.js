4a100fe780abcee217070dd8fe8a8887b1860e29
Sapna190 <sapnasharma8849711@gmail.com>
2024-05-29 23:25:53 +0530

    Updated TextForm component for improved validation

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,13 +3,16 @@ import React,{ useState } from 'react'
 export default function TextForm() {
 
   return (
+
+
+
     <div>
         <div className="mb-3">
             <h2 className='heading my-3 ' >TextUtils to analyze your text.</h2>
             <label htmlFor="exampleFormControlTextarea1" className="form-label">Example</label>
             <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            <button>button1</button>
-            <button>button2</button>
+            <button type="button" class="btn btn-primary"  onClick={toUppercase}>Uppercase</button>
+            <button type="button" class="btn btn-primary" onClick={toLowercase}>Lowercase</button>
         </div>
     </div>
   )
